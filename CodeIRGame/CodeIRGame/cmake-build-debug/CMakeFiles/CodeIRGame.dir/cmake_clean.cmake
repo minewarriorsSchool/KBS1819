6ae423d0cc14e7458272bb98e02dd3bfa0143f8c
Jelle van Urk <s1114436@student.windesheim.nl>
2018-11-22 18:16:45 +0100

    basic map structure update

---
+++ CodeIRGame/CodeIRGame/cmake-build-debug/CMakeFiles/CodeIRGame.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CodeIRGame.dir/main.cpp.obj"
+  "CodeIRGame.pdb"
+  "CodeIRGame.exe"
+  "CodeIRGame.exe.manifest"
+  "libCodeIRGame.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CodeIRGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CodeIRGame/CodeIRGame/cmake-build-debug/CMakeFiles/CodeIRGame.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CodeIRGame.
+# This may be replaced when dependencies are built.
