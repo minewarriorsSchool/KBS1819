9ddeae1b8e2326ec43c2350ba73ca49ac6de52ef
Jelle van Urk <s1114436@student.windesheim.nl>
2018-12-05 15:50:46 +0100

    Fast PWM mode registers

--- IRGame/Main/core/Main.ino
+++ IRGame/Main/core/Main.ino
@@ -1,6 +1,10 @@
 #include <Arduino.h>
 
 //Declaring registers here
+/* going for fast PWM mode so Fpwm = Fclk I:O / N * 256 --> 56khz  && 32khz wanted
+*/
+
+TCCR2A = (1<<0) | (1<<0) // Fast PWM mode
 
 //End declaring registers
 
