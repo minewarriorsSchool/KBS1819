090b5e2007ee4da86c00de5baaeb3a2eefad9283
Jelle van Urk <s1114436@student.windesheim.nl>
2018-12-05 17:13:12 +0100

    no message

--- IRGame/Main/core/Main.ino
+++ IRGame/Main/core/Main.ino
@@ -1,7 +1,9 @@
 #include <Arduino.h>
 
 //Declaring registers here
-/* going for fast PWM mode so Fpwm = Fclk I:O / N * 256 --> 56khz  && 32khz wanted
+/* going for fast PWM mode
+16.000.000/56.000/8 close to 36 (35,714285714285714285714285714286)
+16.000.000/38.000/8 close to 53 (52,631578947368421052631578947368)
 */
 
 TCCR2A = (1<<WGM21) | (1<<WGM20) // Fast PWM mode TOP: 0xFF, Update of OCRx at Bottom, TOV flag set on MAX
