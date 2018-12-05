b79ebc662ed236fb7b692a27883cf9ebec1a442d
Jelle van Urk <s1114436@student.windesheim.nl>
2018-12-05 17:36:47 +0100

    info in comments for scaling

--- IRGame/Main/core/Main.ino
+++ IRGame/Main/core/Main.ino
@@ -1,13 +1,22 @@
 #include <Arduino.h>
 
 //Declaring registers here
-/* going for fast PWM mode
+/* 
+going for fast PWM mode
 16.000.000/56.000/8 close to 36 (35,714285714285714285714285714286)
 16.000.000/38.000/8 close to 53 (52,631578947368421052631578947368)
 */
 
-TCCR2A = (1<<WGM21) | (1<<WGM20) // Fast PWM mode TOP: 0xFF, Update of OCRx at Bottom, TOV flag set on MAX
-TCCR2B = (1<<CS22) | (1<<CS20) // prescaling 128
+/*
+Clear OC2B on Compare match, set OC2B at bottom, (non-inverting mode)
+Fast PWM mode TOP: 0xFF, Update of OCRx at Bottom, TOV flag set on MAX
+*/
+TCCR2A |= (1<<COM2B1)| (1<<WGM21) | (1<<WGM20);
+//pre-scaling 8
+TCCR2B |= (1<<CS20);
+/* OCR2B = 53 if sender is 38 KHZ
+   OCR2b = 36 if sender is 56 KHZ
+*/
 
 //End declaring registers
 
