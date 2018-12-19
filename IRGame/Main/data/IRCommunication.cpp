fa9a0db74695436b2f822be085994d737a17b6e0
Jelle van Urk <s1114436@student.windesheim.nl>
2018-12-19 16:24:07 +0100

    Encoding works LED send works

--- IRGame/Main/data/IRCommunication.cpp
+++ IRGame/Main/data/IRCommunication.cpp
@@ -4,6 +4,7 @@
 
 #include "IRCommunication.h"
 
+//Constructor
 IRCommunicatie::IRCommunicatie(uint8_t frequency, boolean testMode){
 	while(frequency!=frequency56kHz  && frequency!=frequency38kHz){}	//Making sure that no other frequency is allowed besides required frequencies4
 	
@@ -22,33 +23,71 @@ IRCommunicatie::IRCommunicatie(uint8_t frequency, boolean testMode){
 		}else OCR2B = OCR2BWaarde56kHz;
 	}
 }
+//End-Constructor
 
+//Functies
 void IRCommunicatie::setHzfrequency(){
-	PORTB |= (1<<PORTB5);												//Test LED PORT to high pin 13
+	PORTB |= (1<<PORTB5);												//if LED 13 is high, IR can not send
 	DDRB |= (1<<DDB5);													//Test LED to output pin 13
 	PORTD |= (1<<PORTD3);												//PWM PIN 3 infra rood to high
 	DDRD |= (1<<DDD3);													//PWM PIN 3 infra rood to output
 	TCCR2A |= (1<<COM2A0)| (1<<COM2B1) | (1<<WGM20) | (1<<WGM21);		//set compare B
 	TCCR2B |= (1<<CS21) | (1<<WGM22);									//set clock pre-scaler to 8 and Fast PWM
 	TIMSK2 |= (1<<TOIE2);
+
+	//Debug encoding BIT to DATA
+	encodingToTime(dummyVariable);
+	//end-debug
+	
 	sei();																// enable global interrupts
 }
 
-void IRCommunicatie::changeCounter(boolean pinchange){					//on pinchange reset counter else keep counting overflows
-	if(pinchange == true){												//if there is a pinchange reset the counter
-		setCounterToZero();												//resetting counter
-		PORTB ^= (1<<PORTB5);											//blink a LED to see the pinchange
-		} else if(getCounter() == OverFlowCounterStopBit){				//if overflow is equal to stop-bit, stop adding overflows to counter and wait till pinchange to reset counter
-		
-		}else{															//else add 1 overflow to overflowcounter
-		counter++;														//up counter with 1
+void IRCommunicatie::counterPlusOne(){
+	counter++;
+}
+
+void IRCommunicatie::encodingToTime(int *Byte){
+	for (int i=0; i<=7; i++)
+	{
+		if(Byte[i] == 1){
+			dummyTimes[i] = OverFlowCounterBit1;
+			} else if(Byte[i] == 0){
+			dummyTimes[i] = OverFlowCounterBit0;
+		}else Serial.println("Setting BIT to TIME error");
+	}
+	for (int i=0; i<=7; i++)
+	{
+		Serial.println((dummyTimes[i] ));
 	}
 }
 
+void IRCommunicatie::encodetimeToLED(int *Times){
+
+	if(nextBit){
+		PORTB ^= (1<<PORTB5);
+		setCounterToZero();
+		nextBit = false;
+	}
+	
+	if(getCounter() == Times[bitCounter]){
+		nextBit = true;
+		Serial.print("Counter: ");
+		Serial.println(getCounter());
+		bitCounter++;
+		if(bitCounter==7) bitCounter = 0;
+	}
+}
+
+//End-Functies
+
+//Getters
 int IRCommunicatie::getCounter(){
 	return counter;
 }
+//End-Getters
 
+//Setters
 void IRCommunicatie::setCounterToZero(){
 	counter = 0;
 }
+//End-Setters
--- IRGame/Main/data/IRCommunication.h
+++ IRGame/Main/data/IRCommunication.h
@@ -8,11 +8,10 @@
 #define OCR2BWaarde38kHz 26 // 50.75% 37,71kHz
 #define frequency56kHz 56	// definition of 56 kHz
 #define frequency38kHz 38	// definition of 38 kHz
-#define OverFlowCounterBit0 20
-#define OverFlowCounterBit1 30
-#define OverFlowCounterStartBit 50
-#define OverFlowCounterStopBit 60
-#define OverFlowCounterParity 40
+#define OverFlowCounterBit0 10000
+#define OverFlowCounterBit1 20000
+#define OverFlowCounterStartBit 300
+#define OverFlowCounterStopBit 400
 
 #if defined(ARDUINO) && ARDUINO >= 100
 #include "arduino.h"
@@ -26,23 +25,24 @@ class IRCommunicatie{
 	IRCommunicatie(uint8_t frequency, boolean testMode);
 	
 	//Functies
+	void counterPlusOne();
 	void setHzfrequency();
-	void changeCounter(boolean pinchange);
+	void encodingToTime(int *Byte);
+	void encodetimeToLED(int *Times);
 	
 	//Getters
 	int getCounter();
-	
 	//Setters
 	void setCounterToZero();
 	
 	//+Variabelen
-	
+	int dummyTimes[8];
 	protected:
 	private:
 	
 	//-Variabelen
-	int counter = 0;
-	
+	int counter = 0, bitCounter = 0, dummyVariable[8] = {0,0,1,1,0,0,0,0};
+	boolean nextBit = true;
 };
 
 
